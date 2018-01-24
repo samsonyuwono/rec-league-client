a2d863ea0656f1d105951a92dacc96849c7058ef
github username <github email address>
2018-01-24 15:05:34 -0500

    testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,11 +12,11 @@ const Home = (props) => {
       </div>
       <div className="padding-bottom">
        <Auth />
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/xEew8pSCZCw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/xEew8pSCZCw" frameBorder="0" allow="autoPlay; encrypted-media" allowFullScreen></iframe>
      </div>
       <div className="padding-top">
         <p className="padding-top">Manage your teams and players all in one app</p>
-        <p> This is an open sourced project, so if you would like to contribute please go to my        <a href="https://github.com/samsonyuwono/nba-team-search" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
+        <p> This is an open sourced project, so if you would like to contribute please go to my        <a href="https://github.com/samsonyuwono/rec-league-app" target="_blank" rel="noopener noreferrer"> Github Page</a></p>
         <p>Created by <a href="https://github.com/samsonyuwono/" target="_blank" rel="noopener noreferrer">Samson Yuwono</a> as a project at the Flatiron School</p></div>
       </div>
   )
--- src/components/players/PlayerCard.js
+++ src/components/players/PlayerCard.js
@@ -9,6 +9,7 @@ const PlayerCard = ({ team, player }) => (
     <p>Weight(lb): {player.weight} </p>
     <img className="PlayerImage" src={player.image_url} alt={player.name} />
     <div>
+    <Link style={{ marginBottom: '5px'}} to={`/players/${player.id}`}>View Player</Link> <br />
       <Link style={{ marginBottom: '5px'}} to={`/players/${player.id}/edit`}>Edit Player</Link> <br />
       <PlayerLike player={player.id} likes={player.likes}/>
     </div>
