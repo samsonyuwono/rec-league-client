7445e1c9eac7cbfdadf2f741504cb67c88746cd9
github username <github email address>
2018-01-25 21:07:41 -0500

    for deploy

--- src/actions/teams.js
+++ src/actions/teams.js
@@ -1,6 +1,10 @@
 import { resetTeamForm } from './teamForm';
 
-const API_URL = process.env.REACT_APP_API_URL;
+let API_URL = process.env.REACT_APP_API_URL;
+
+if (process.env.NODE_ENV === "production"){
+  process.env.REACT_PRODUCTION_API_URL;
+}
 
 const setTeams = teams => {
   return {
